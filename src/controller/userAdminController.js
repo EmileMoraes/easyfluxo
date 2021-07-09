1796d1e04761148e2acbafc85af1c7f2beefb0b1
Emile <moraesemile@gmail.com>
2021-07-08 21:46:28 -0300

    Configurado a database

--- src/controller/userAdminController.js
+++ src/controller/userAdminController.js
@@ -29,21 +29,21 @@ const createUsuario = async (req, res) => {
 
     const { nome, email, senha, isAdmin } = req.body
 
-    if(email == "" || senha == "" || nome == "" || isAdmin == ""){
-        return res.status(400).json({message: 'Favor preencher todos campos'})
+    if (email == "" || senha == "" || nome == "" || isAdmin == "") {
+        return res.status(400).json({ message: 'Favor preencher todos campos' })
     }
 
-   const validaEmail = utils.validadeEmail(email)
-   if(!validaEmail){
-       return res.status(400).json({message: 'Favor inserir um email valido'})
-   }
+    const validaEmail = utils.validadeEmail(email)
+    if (!validaEmail) {
+        return res.status(400).json({ message: 'Favor inserir um email valido' })
+    }
     const userExiste = await userAdminDB.findOne({ email: req.body.email })
     if (userExiste) {
         return res.status(409).json({ error: 'User já existe' })
     }
     const usuario = await userAdminDB.create(req.body)
     const novoUsuario = usuario.save()
-    return res.status(201).json({message: 'Novo usuario criado com sucesso:', usuario})
+    return res.status(201).json({ message: 'Novo usuario criado com sucesso:', usuario })
 
 }
 
@@ -76,8 +76,8 @@ const deleteUsuario = async (req, res) => {
 const login = async (req, res) => {
     const { email, senha } = req.body
 
-    if(email == "" || senha == "" || senha == undefined){
-        return res.status(400).json({message: 'Favor preencher os campos e-mail e senha '})
+    if (email == "" || senha == "" || senha == undefined) {
+        return res.status(400).json({ message: 'Favor preencher os campos e-mail e senha ' })
     }
 
     const usuario = await userAdminDB.findOne({ email: req.body.email })
@@ -90,10 +90,13 @@ const login = async (req, res) => {
     if (!senhaValida) {
         return res.status(403).send(`Senha invalida ${senhaValida}`);
     }
+    try {
+        const token = jwt.sign({ email: req.body.email }, SECRET);
 
-    const token = jwt.sign({ email: req.body.email }, SECRET);
-
-    return res.status(200).json({message: 'Token ', token});
+        return res.status(200).json({ message: 'Token ', token });
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
 }
 
 module.exports = {
